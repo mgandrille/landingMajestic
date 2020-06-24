d5ff4ed870ed909a3d6a65829be4cc4626d78e0e
Marie Gandrille <mgandrille@yahoo.fr>
2020-06-24 16:36:27 +0200

    Actions sur formulaire

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+
+var_dump($_SESSION);
+?>
+
 <?php include 'partials/header.php' ; ?>
 
 <main class="container-fluid px-0">
@@ -10,6 +16,13 @@
         </div>
         <div class="col-3 px-5 py-5">
             <div class="my-4">
+                <?php if (isset($_SESSION['form_error']) &&  $_SESSION['form_error'] === true) {
+                    unset($_SESSION['form_error']); ?>
+                    <p class="lead text-danger">Une erreur est survenue à l'envoi du formulaire.</p>
+                <?php }
+                else { ?>
+                    <p class="lead text-success">Votre inscription a bien été enregistrée !</p>
+                <?php } ?>
                 <h6 class="text-wrap">Pour être informé en avant-première <br> Inscrivez-vous dès maintenant :</h6>
             </div>
             <form action="traitement.php" method="post" class="form">
@@ -25,7 +38,7 @@
                 <div class="form-group">
                     <input type="tel" name="telephone" class="form-control rounded-0 my-4" id="telephone" placeholder="Numéro de tel" pattern="[0-9]{10}" required>
                 </div>
-                <input type="button" class="btn" value="Valider">
+                <input type="submit" class="btn" value="Valider">
             </form>
         </div>
     </div>
